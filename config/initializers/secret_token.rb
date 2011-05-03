a59db6600655efa8beeede0b8fb84fcc99a29916
Brenda Strech <bstrech@enphaseenergy.com>
2011-05-02 21:09:03 -0700

    Initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JudoStudentTracker::Application.config.secret_token = '4507ddad6feb6443a23bda113a2d1e9d7dccf8098d586b16352463a4fa813d9298959381d84e905612c51901c3509c7aa3dc735b3e0515aa5bcd5b1bef953eec'
