55dc71975a25c2c4c3daa17771c109f10cc320ec
bstrech <bstrech@enphaseenergy.com>
2013-07-02 10:04:03 -0700

    Upgraded to ruby 2.0 and rails 4.0.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-JudoStudentTracker::Application.config.secret_token = '4507ddad6feb6443a23bda113a2d1e9d7dccf8098d586b16352463a4fa813d9298959381d84e905612c51901c3509c7aa3dc735b3e0515aa5bcd5b1bef953eec'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JudoStudentTracker::Application.config.secret_key_base = '5a492a791d41e10523e30b53afb037b45e3657da0d09b917d99c5344278f1c8f2a903a4c932281af70fdf56ce5f6cb241fb53e74c819a428be1fe820d83f2552'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-JudoStudentTracker::Application.config.session_store :cookie_store, :key => '_judo_student_tracker_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# JudoStudentTracker::Application.config.session_store :active_record_store
+JudoStudentTracker::Application.config.session_store :cookie_store, key: '_judo_student_tracker_session'
